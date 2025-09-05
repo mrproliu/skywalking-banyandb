2b236a1d92a358969aa394cbabb3542d25d82852
mrproliu <741550557@qq.com>
2025-09-06 00:30:36 +0800

    update

--- test/stress/stable/generator.go
+++ test/stress/stable/generator.go
@@ -105,7 +105,7 @@ func (b *baseGenerator[Request]) generateData(owner generatorCallback) {
 	start := time.Now()
 
 	//fast write next 1h data
-	for i := range 60 * 24 * 3 {
+	for i := range 60 * 24 * 2 {
 		//for i := range 1 {
 		fmt.Println("starting fast write next 1 minute data, index: ", i)
 		b.generateDataFromFileStart(downstream)
@@ -121,7 +121,7 @@ func (b *baseGenerator[Request]) generateData(owner generatorCallback) {
 	}
 
 	duration := time.Now().Sub(start)
-	fmt.Println("fast write next 3d data finished, duration:", duration)
+	fmt.Println("fast write next 2d data finished, duration:", duration)
 
 	var shouldSkipWaitNextMinute bool
 	for {
